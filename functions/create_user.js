d02af780fac3ff7aee9752de238bc1a1e7a67822
Carl Hain <crhain@gmail.com>
2019-02-27 21:02:03 -0600

    Finish create_user cloud function

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,21 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  res.send(req.body);
+  // Verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  // Format the phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, '');
+
+  // Create a new user accountusing that phone number
+  admin
+    .auth()
+    .createUser({ uid: phone })
+    .then(user => {
+      // Respond to the user request, saying the account was made
+      res.send(user);
+    })
+    .catch(err => res.status(422).send({ error: err }));
 };
