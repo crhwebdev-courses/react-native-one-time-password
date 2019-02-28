6c91998f4be61b47ddca1bd95b78e779fbbec1a0
Carl Hain <crhain@gmail.com>
2019-02-28 15:53:19 -0600

    Add event handlers that make ajax requests to existing cloud functions to SignUpForm

--- firebase-auth/components/SignUpForm.js
+++ firebase-auth/components/SignUpForm.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Input, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL =
+  'https://us-central1-one-time-password-1c34f.cloudfunctions.net';
 
 class SignUpForm extends Component {
+  state = { phone: '' };
+
+  // useing es2017 syntax to bind this to this component
+  // instead of using this.handleSubmit.bind(this) when passing
+  // as callback
+  handleSubmit = () => {
+    axios
+      .post(`${ROOT_URL}/createUser`, { phone: this.state.phone })
+      .then(() => {
+        axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+          phone: this.state.phone
+        });
+      });
+  };
+
   render() {
     return (
       <View>
         <View style={{ marginBottom: 20 }}>
           <Text>Enter Phone Number</Text>
-          <Input />
+          <Input
+            value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
         </View>
 
-        <Button title="Submit" />
+        <Button title="Submit" onPress={this.handleSubmit} />
       </View>
     );
   }
