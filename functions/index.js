3413ee6df28225e418d23395ff599ea8b29bb6cd
Carl Hain <crhain@gmail.com>
2019-02-27 20:32:44 -0600

    Set up initialization of firebase-admin with credentials for firebase app in index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const serviceAccount = require('./service_account.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://one-time-password-1c34f.firebaseio.com'
+});
+
 exports.createUser = functions.https.onRequest(createUser);
