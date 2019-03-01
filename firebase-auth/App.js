21071105702e5b2258081c1cb80744be441509e5
Carl Hain <crhain@gmail.com>
2019-03-01 09:58:26 -0600

    Style App.js to add space around SignUpForm and SignInForm

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -19,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-around'
   }
 });
